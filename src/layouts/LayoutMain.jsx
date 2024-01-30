d4fe2b59894731094b2a3e025ca7b7a9522d4fd4
alexmoon26 <krasava_team@mail.ru>
2024-01-30 10:50:19 +0300

    Revert "Update LayoutMain.jsx"
    This reverts commit 00286087388e2bedf5c09946a8f52813657820b4.

--- src/layouts/LayoutMain.jsx
+++ src/layouts/LayoutMain.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
-import { Header } from "../components/header/Header.jsx";
-import { Footer } from "../components/footer/Footer.jsx";
+import { Header } from "../components/header/Header";
+import { Footer } from "../components/footer/Footer";
 import { Box } from "@mui/material";
 import { useScreenWidth } from "../hooks/useScreenWidth";
 
